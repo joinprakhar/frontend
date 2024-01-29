de9277007c8a25e3000f4f45361b5aece0103133
joinprakhar <pandeylive@gmail.com>
2024-01-29 18:57:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Carousel from "./Component/Small Components/Coursel";
 import MinorProject from "./Component/Small Components/MinorProject";
+import ProjectDetailModal from "./Component/Small Components/ProjectDetailModal";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ProjectDetailModal />
+    </div>
+  );
 }
 
 export default App;
--- src/Component/Small Components/ProjectDetailModal.jsx
+++ src/Component/Small Components/ProjectDetailModal.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import "../CSS/ProjectDetailModal.css";
+import Carousel from "./Coursel";
 
 const ProjectDetailModal = () => {
-  return <div>ProjectDetailModal</div>;
+  const imgs = [
+    { link: "" },
+    { link: "" },
+    { link: "" },
+    { link: "" },
+    { link: "" },
+    { link: "" },
+  ];
+
+  const url =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtR6A_5G9ik5IY2Vqs-sjkyx32Eo_igLYBDA&usqp=CAU";
+
+  return (
+    <div className="project-detail-box">
+      <div className="project-detail-image-box">
+        <img src={url} alt="" />
+        <div className="project-detail-image-carousel">
+          <Carousel>
+            {imgs &&
+              imgs.map((ele) => {
+                return (
+                  <img
+                    style={{
+                      width: "70px",
+                      height: "70px",
+
+                      marginRight: "10px",
+                    }}
+                    src={url}
+                    alt={""}
+                  />
+                );
+              })}
+          </Carousel>
+        </div>
+      </div>
+      <div className="project-detail-detail-box"></div>
+    </div>
+  );
 };
 
 export default ProjectDetailModal;
