be087fe0213514dd2b0c677ee79d772159d5ddab
joinprakhar <pandeylive@gmail.com>
2024-03-01 16:19:06 +0530

    small changes

--- src/Component/Small Components/Coursel.jsx
+++ src/Component/Small Components/Coursel.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import "../../App.css";
 
 const Carousel = ({ arrow, children }) => {
   const boxRef = useRef(null);
@@ -18,57 +19,38 @@ const Carousel = ({ arrow, children }) => {
     }
   };
 
-  const handleScroll = (event) => {
-    // Get the current scroll position
-    const scrollLeft = event.target.scrollLeft;
-
-    // Determine the scroll direction
-    if (scrollLeft > prevScrollLeft.current) {
-      // Scrolling to the right
-      if (boxRef.current) {
-        const width = boxRef.current.clientWidth;
-        boxRef.current.scrollLeft += width / 2;
-      }
-    } else {
-      // Scrolling to the left
-      if (boxRef.current) {
-        const width = boxRef.current.clientWidth;
-        boxRef.current.scrollLeft -= width / 2;
-      }
-    }
-
-    // Update the previous scroll position
-    prevScrollLeft.current = scrollLeft;
-  };
-
   return (
-    <div className="product-carousel">
-      <div
-        onClick={btnPressPrev}
-        style={{
-          padding: "0",
-          margin: "0 10px",
-          fontSize: "20px",
-          color: "white",
-        }}
-      >
-        <i class="fa fa-toggle-left" style={{ color: arrow }}></i>
+    <>
+      <div className="product-carousel">
+        <div
+          onClick={btnPressPrev}
+          className="caro-arrow"
+          style={{
+            padding: "0",
+            margin: "0 10px",
+            fontSize: "20px",
+            color: "white",
+          }}
+        >
+          <i class="fa fa-toggle-left" style={{ color: arrow }}></i>
+        </div>
+        <div className="product-container" ref={boxRef}>
+          {children}
+        </div>
+        <div
+          onClick={btnPressNext}
+          className="caro-arrow"
+          style={{
+            padding: "0",
+            margin: "0 10px",
+            fontSize: "20px",
+            color: "white",
+          }}
+        >
+          <i class="fa fa-toggle-right" style={{ color: arrow }}></i>
+        </div>
       </div>
-      <div className="product-container" ref={boxRef}>
-        {children}
-      </div>
-      <div
-        onClick={btnPressNext}
-        style={{
-          padding: "0",
-          margin: "0 10px",
-          fontSize: "20px",
-          color: "white",
-        }}
-      >
-        <i class="fa fa-toggle-right" style={{ color: arrow }}></i>
-      </div>
-      {/* <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <div className="arrow-cont">
         <div
           onClick={btnPressPrev}
           style={{
@@ -91,8 +73,8 @@ const Carousel = ({ arrow, children }) => {
         >
           <i class="fa fa-toggle-right"></i>
         </div>
-      </div> */}
-    </div>
+      </div>
+    </>
   );
 };
 
